b8c2c0ff88a136c3e37f2863ad621eba2e879d9b
Dan Ledwith <dledw001@odu.edu>
2025-04-18 01:26:39 -0400

    updating LogDetail.java

--- src/main/java/com/logs/LogDetail.java
+++ src/main/java/com/logs/LogDetail.java
@@ -1,5 +1,36 @@
 package com.logs;
 
+import java.util.UUID;
+import java.time.Instant;
+
 public class LogDetail {
-  
+  private UUID logId;
+  private Instant createInstant;
+  private double value;
+
+  // New LogDetail constructor
+  public LogDetail(UUID logId, double value) {
+    this.logId = logId;
+    this.value = value;
+    this.createInstant = Instant.now();
+  }
+
+  // Public getters
+  public UUID getLogId() {
+    return this.logId;
+  }
+
+  public Instant getCreateInstant() {
+    return this.createInstant;
+  }
+
+  public double getValue() {
+    return this.value;
+  }
+
+  // Public toString method
+  public String toString() {
+    String string = "%s,%s,%s".formatted(this.logId, this.createInstant, this.value);
+    return string;
+  }
 }
